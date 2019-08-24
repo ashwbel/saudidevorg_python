89cd535f65fcc0190f3ea1b5fdef9690940d4b69
ashwbel <ashwag.wabel11@gmail.com>
2019-08-25 01:02:25 +0300

    This Is My Code For DAY 4 !!

---
+++ Day4.py
@@ -0,0 +1,86 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+x=1
+y=3.44
+z=4j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#int
+x=33
+y=288366366663
+z=-877
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Float
+x=3.544
+y=1.98
+z=-6.77
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+#Float
+x=35e44
+y=19E8
+z=-6.7e7
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+#Complex
+x=3+6j
+y=6j
+z=-8j
+
+
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+x=88
+y=4.56
+z=6j
+
+
+#from int to Float
+a=float(x)
+#from Float to int
+b=int(y)
+#from int to Complex
+c=complex(z)
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+
+
+
+import random
+
+
+print(random.randrange(1,10))
