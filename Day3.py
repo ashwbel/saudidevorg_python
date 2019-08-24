f7886eae735139ba3f857f426397abb8f63d18f9
ashwbel <ashwag.wabel11@gmail.com>
2019-08-24 22:31:41 +0300

    This Is My Code For DAY 3 !!

---
+++ Day3.py
@@ -0,0 +1,34 @@
+x= 5
+
+y="ashwag"
+
+print(x)
+print(y)
+
+
+a,b,c =10
+print(a)
+print(b)
+print(c)
+
+
+
+z="ashwag"
+
+print('Hello '+ z)
+
+
+m= "Hello "
+n= "World"
+l=m+n
+print(l)
+
+
+o= 5
+s= 2
+print(o+s)
+
+
+A=11
+B="ahmad"
+print(A+B)
