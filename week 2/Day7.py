7531596409c215f8d46293392c00906d3f1eae91
ashwbel <ashwag.wabel11@gmail.com>
2019-08-26 17:09:16 +0300

    This Is My Code For DAY 7 !!

---
+++ week 2/Day7.py
@@ -0,0 +1,34 @@
+#day 7
+
+#Exercise 1
+#print
+
+
+print("hello")
+print('ashwag')
+
+
+#Exercise 2
+#print_varible
+x="ashwag"
+
+print(x)
+
+
+#Exercise 3
+
+
+x="""Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+print(x)
+
+
+#Exercise 4
+
+a="Hello World"
+print (a[3])
+print (a[2:5])
