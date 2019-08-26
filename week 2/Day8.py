aa6546b34623fc132b94f7ba1535902be439dfb2
ashwbel <ashwag.wabel11@gmail.com>
2019-08-26 17:20:37 +0300

    This Is My Code For DAY 8 !!

---
+++ week 2/Day8.py
@@ -0,0 +1,45 @@
+#day 8
+
+#Exercise 1
+#split fun
+
+x="hello ashwag  "
+
+print(x.strip())
+
+
+#Exercise 2
+#len fun
+x="hello erveryone "
+
+print(len(x))
+
+
+#Exercise 3
+#lower fun
+x="hello erveryone "
+
+print(x.lower())
+
+
+#Exercise 4
+#upper fun
+x="hello erveryone "
+
+print(x.upper())
+
+
+#Exercise 5
+#replace fun
+x="hello erveryone "
+
+print(x.replace("e","f"))
+
+
+
+
+#Exercise 6
+#split fun
+x="hello, erveryone "
+
+print(x.split(","))
