268fb041a2ec6732d60a0ad44653b414e34c5d20
ashwbel <ashwag.wabel11@gmail.com>
2019-08-26 16:56:05 +0300

    This Is My Code For DAY 6  !!

---
+++ week 2/Day6.py
@@ -0,0 +1,38 @@
+#day 6
+
+#Exercise 1
+#to int
+
+x=int(1)
+y=int(3.4)
+z=int("3")
+
+#print
+print(x)
+print(y)
+print(z)
+
+#Exercise 2
+#to Float
+
+x=float(6.2)
+y=float(3)
+z=float("3.5")
+
+#print
+print(x)
+print(y)
+print(z)
+
+
+#Exercise 3
+#to string
+
+x=str("ash")
+y=str(9)
+z=str("4.5")
+
+#print
+print(x)
+print(y)
+print(z)
