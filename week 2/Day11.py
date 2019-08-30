3b85ddfd53a283c2ea9a348b004578d3b61cf1f5
ashwbel <ashwag.wabel11@gmail.com>
2019-08-30 10:22:11 +0300

    This Is My Code For DAY 11 !!

---
+++ week 2/Day11.py
@@ -0,0 +1,29 @@
+#day 11
+
+#Exercise 1
+
+
+x=7
+
+print(x > 3 or x < 9 )
+
+
+
+#Exercise 2
+
+x='ahmad'
+y='Ali'
+z=x
+
+print(x is not z)
+
+
+print(x is not y)
+
+
+
+#Exercise 3
+
+x='ashwag'
+
+print('ash' in x )
