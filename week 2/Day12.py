5deb45822821234f3b6df41e6c3222583981ff1b
ashwbel <ashwag.wabel11@gmail.com>
2019-08-30 10:23:08 +0300

    This Is My Code For DAY 12 !!

---
+++ week 2/Day12.py
@@ -0,0 +1,21 @@
+stant = 'Please, I want {} sandwishes and {} donutes '
+
+
+
+#Exercise 1
+stant=stant.replace("I","we")
+
+
+#Exercise 2
+x=5
+y= 7
+stant=stant.format(x ,y)
+
+
+
+#Exercise 3
+m= 'a'
+stant=stant.replace('a', m.upper())
+
+
+print(stant)
