8d158246cf32d1f780c84fe287d0c70618884b9e
ashwbel <ashwag.wabel11@gmail.com>
2019-08-30 10:21:12 +0300

    This Is My Code For DAY 10 !!

---
+++ week 2/Day10.py
@@ -0,0 +1,46 @@
+#day 10
+
+#Exercise 1
+
+
+x=10
+y=3
+
+
+print(x + y )
+print(x * y )
+print(x % y )
+print(x - y )
+
+
+#Exercise 2
+
+x=10
+
+x/=2
+print(x)
+
+###################3
+x=15
+
+x*=2
+print(x)
+
+###################3
+x=3
+
+x+=6
+print(x)
+
+#Exercise 3
+
+x=10
+
+y=7
+
+
+print(x > y)
+
+print(x == y)
+
+print(x < y)
