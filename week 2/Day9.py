f0e5eeeb5dcca874987207d1a367aeb8dd2df9c3
ashwbel <ashwag.wabel11@gmail.com>
2019-08-30 10:20:36 +0300

    This Is My Code For DAY 9  !!

---
+++ week 2/Day9.py
@@ -0,0 +1,25 @@
+#day 9
+
+#Exercise 1
+
+age= 33
+x="hello my age is {} "
+
+print(x.format(age))
+
+
+#Exercise 2
+age= 33
+Height =160
+x="hello my age is{} my Height {}  "
+
+print(x.format(age ,Height))
+
+
+#Exercise 3
+
+age= 33
+Height =160
+x="hello my Height is{1} my age {0}  "
+
+print(x.format(age ,Height))
