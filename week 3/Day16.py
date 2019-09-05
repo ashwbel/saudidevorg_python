6c4bc33a50686cd5a50018582931f91ca7c17c9f
ashwbel <ashwag.wabel11@gmail.com>
2019-09-05 10:37:22 +0300

    This Is My Code For DAY 16 !!

---
+++ week 3/Day16.py
@@ -0,0 +1,27 @@
+#Exercise 1
+tup =(1,3,4,5,6)
+print(tup)
+
+
+#Exercise 2
+tup =(1,)
+print(tup)
+
+#Exercise 3
+tup =(1,5.23,1.2,3.8,3,4,5,6)
+print(tup)
+
+
+#Exercise 4
+tup =(1,5.23,1.2,3.8,3,4,5,6)
+print(tup[4])
+
+#Exercise 5
+wordtup  =('hello','how','why' ,2,4,1)
+for x in wordtup:
+    print(x)
+
+
+#Exercise 6
+tup =(1,5.23,1.2,3.8,3,4,5,6)
+print(tup[4:6])
