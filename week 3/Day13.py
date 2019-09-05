7aa154eaf2b5f956468fe4883c61188cb1858dee
ashwbel <ashwag.wabel11@gmail.com>
2019-09-05 10:19:13 +0300

    This Is My Code For DAY 13 !!

---
+++ week 3/Day13.py
@@ -0,0 +1,52 @@
+
+
+
+#Exercise 1
+num=[1,3,4,5,6]
+print(num)
+
+
+#Exercise 2
+word  =['hello','how','why']
+print(word)
+
+
+
+#Exercise 3
+numchr= ['hello','how','why',3,4,5]
+print(numchr)
+
+
+#Exercise 4
+flot = [3.44,5.23,1.2,3.8,4.9,5.0]
+print(flot)
+
+
+#Exercise 5
+word  =['hello','how','why']
+print(word[2])
+
+
+
+#Exercise 6
+numchr= ['hello','how','why',3,4,5]
+for x in numchr:
+   print(x)
+
+
+#Exercise 7
+word  =['hello','how','why']
+word[2]='ashwag'
+print(word)
+
+
+#Exercise 8
+word  =['hello','how','why']
+del word[1]
+print(word)
+
+
+
+#Exercise 9
+word  =['hello','how','why']
+del word
