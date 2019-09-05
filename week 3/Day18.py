ce76f0b89b24ffca1285b8b994a581875e2c658f
ashwbel <ashwag.wabel11@gmail.com>
2019-09-05 16:38:35 +0300

    This Is My Code For DAY 18!!

---
+++ week 3/Day18.py
@@ -0,0 +1,26 @@
+
+
+#Exercise 1
+num=[1,3,4,5,6]
+num.append(7)
+print(num)
+
+
+#Exercise 2
+word  = ['hello','how','why']
+wordc=word.copy()
+print(wordc)
+
+
+#Exercise 3
+num2=[1,6,4,8,2,8,9,3]
+num2.sort()
+print(num2)
+
+
+#Exercise 4
+word  = ['hello','how','why']
+word.pop()
+print(word)
+word.clear()
+print(word)
