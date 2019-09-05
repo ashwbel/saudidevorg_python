d80dd12f7a5ad633d0d50f647d88df9aeaa39fb9
ashwbel <ashwag.wabel11@gmail.com>
2019-09-05 10:19:57 +0300

    This Is My Code For DAY 14 !!

---
+++ week 3/Day14.py
@@ -0,0 +1,26 @@
+
+
+
+#Exercise 1
+num=[1,3,4,5,6]
+print(num[2:4])
+
+
+#Exercise 2
+word  =['hello','how','why']
+if 'hello' in word:
+    print'helo every one'
+
+
+#Exercise 3
+numchr= ['hello','how','why',3,4,5]
+word  =['my','name','ashwag']
+sumof2list= numchr+ word
+print(sumof2list)
+
+
+#Exercise 4
+num=[1,3,4,5,6]
+flot = [3.44,5.23,1.2,3.8,4.9,5.0]
+sum = num+flot
+print(sum)
