60b707580916748bc4c61e2a2554669666b881e4
ashwbel <ashwag.wabel11@gmail.com>
2019-09-05 10:20:52 +0300

    This Is My Code For DAY 15 !!

---
+++ week 3/Day15.py
@@ -0,0 +1,66 @@
+
+
+
+#Exercise 1
+num=[1,3,4,5,6]
+print(len(num))
+
+
+#Exercise 2
+word  = ['hello','how','why']
+word.append('ashwag')
+print(word)
+
+
+#Exercise 3
+word  = ['hello','how','why']
+word.insert(2,'ashwag')
+print(word)
+
+
+#Exercise 4
+word  = ['hello','how','why']
+word.remove('why')
+print(word)
+
+
+
+#Exercise 5
+word  = ['hello','how','ashwag']
+word.pop()
+print(word)
+
+
+
+#Exercise 6
+word  = ['hello','how','ashwag']
+word.pop(2)
+print(word)
+
+
+#Exercise 7
+word  = ['hello','how','ashwag']
+myword=word.copy()
+print(myword)
+
+
+#Exercise 8
+word  = ['hello','how','ashwag']
+myword=word.copy()
+word.pop()
+print(word)
+print(myword)
+
+
+#Exercise 9
+word  = ['hello','how','ashwag']
+myword=word
+word.pop()
+print(word)
+print(myword)
+
+
+#Exercise 8
+word  = ['hello','how','ashwag']
+myword=list(word)
+print(myword)
