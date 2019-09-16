c0c7d7303fea53e1b6c52ee1a1e77e78c7c65be0
ashwbel <ashwag.wabel11@gmail.com>
2019-09-16 20:58:28 +0300

    This Is My Code For DAY 24!!

---
+++ week 4/Day24.py
@@ -0,0 +1,47 @@
+
+
+#day 24
+
+#Exercise 1
+myinfo={
+"Name":"ashwag" ,
+"Age":30 ,
+"City":"Riyadh"
+}
+thisinfo= myinfo.copy()
+
+print (thisinfo)
+
+#Exercise 2
+myinfo={
+"firstcol":{
+"Name":"ashwag" ,
+"Age":30 ,
+"City":"Riyadh"
+},
+"secncol":{
+"Name":"ashwag" ,
+"Age":30 ,
+"City":"Riyadh"
+}
+}
+print (myinfo)
+
+
+#Exercise 3
+
+firstcol={
+"Name":"ashwag" ,
+"Age":30 ,
+"City":"Riyadh"
+}
+secncol={
+"Name":"ashwag" ,
+"Age":30 ,
+"City":"Riyadh"
+}
+myinfo={
+"firstcol":firstcol,
+"secncol":secncol
+}
+print (myinfo)
