5b7390e08860e0cd50399d4124feaa0c6fd2868e
ashwbel <ashwag.wabel11@gmail.com>
2019-09-16 19:39:24 +0300

    This Is My Code For DAY 21!!

---
+++ week 4/Day21.py
@@ -0,0 +1,31 @@
+
+#day 21
+
+#Exercise 1
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+print(len(firset))
+
+
+#Exercise 2
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+firset.remove('wabel')
+firset.discard(4)
+print (firset)
+
+
+#Exercise 3
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+m = firset.pop()
+print (m)
+
+
+#Exercise 4
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+firset.clear()
+print (firset)
+
+
+
+#Exercise 5
+firset= set(({1,4,5,3,4,'ashwag' ,'wabel'}))
+print (firset)
