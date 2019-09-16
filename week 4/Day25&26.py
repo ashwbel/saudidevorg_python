cc327b121b55e54dcbf338f6be0d18dda574186b
ashwbel <ashwag.wabel11@gmail.com>
2019-09-16 21:20:51 +0300

    This Is My Code For DAY 25&26!!

---
+++ week 4/Day25&26.py
@@ -0,0 +1,25 @@
+
+#day 25&26
+
+#Exercise 1
+myset={1,3,5,7,8}
+myset.update([4,8,12])
+myset.discard(8)
+print (myset)
+myset.clear()
+print (myset)
+
+
+#Exercise 2
+thisdic={
+"Name":"pigeon" ,
+"type":"bird" ,
+"skin cover":"feathers"
+}
+
+print(thisdic["type"])
+
+
+thisdic["skin cover"] ="change cover"
+
+print(thisdic)
