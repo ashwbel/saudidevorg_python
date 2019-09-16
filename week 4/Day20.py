ce02566c49d37835f480eee596d22a0f800eceb9
ashwbel <ashwag.wabel11@gmail.com>
2019-09-16 11:58:36 +0300

    This Is My Code For DAY 20!!

---
+++ week 4/Day20.py
@@ -0,0 +1,22 @@
+#day 20
+
+#Exercise 1
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+print (firset)
+
+
+#Exercise 2
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+for x in firset :
+    print (x)
+
+
+#Exercise 3
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+firset.add(7)
+print (firset)
+
+#Exercise 4
+firset={1,4,5,3,4,'ashwag' ,'wabel'}
+firset.update([7,5,'ali'])
+print (firset)
