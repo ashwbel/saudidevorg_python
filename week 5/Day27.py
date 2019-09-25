813347bb665eb99d1f525ad08d509a0c2b715dd9
ashwbel <ashwag.wabel11@gmail.com>
2019-09-25 23:49:34 +0300

    This Is My Code For DAY 27!!

---
+++ week 5/Day27.py
@@ -0,0 +1,57 @@
+
+
+#day 27
+
+#Exercise 1
+a=10
+b=3
+if a > b:
+    print ('a grater than b ')
+
+
+
+#Exercise 2
+a=10
+b=10
+if a > b:
+    print ('a grater than b ')
+elif a== b:
+    print ('a equal to  b ')
+
+
+
+
+#Exercise 3
+a=10
+b=10
+if a > b:
+    print ('a grater than b')
+elif a== b:
+    print ('a equal to  b')
+else:
+    print ('b grater than a')
+
+
+
+
+#Exercise 4
+a=10
+b=2
+if a > b:print ('a grater than b ')
+
+
+#Exercise 5
+a=10
+b=3
+c=22
+if a > b  and  c > b :
+    print ('both condtion true')
+
+
+
+#Exercise 6
+a=10
+b=3
+c=22
+if a > b  or  c > b :
+    print ('one condtion true')
