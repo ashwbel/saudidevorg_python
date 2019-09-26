43fad4e4f982a143ab3c870241218b5d6ca0c8b7
ashwbel <ashwag.wabel11@gmail.com>
2019-09-26 20:06:11 +0300

    This Is My Code For DAY 29!!

---
+++ week 5/Day29.py
@@ -0,0 +1,26 @@
+
+
+#day 29
+
+#Exercise 1
+mylist=[3,4,5,6]
+for x in mylist:
+    print(x)
+
+
+#Exercise 2
+mylist=[3,4,5,6]
+for x in mylist:
+    print(x)
+    if x==5:
+        break
+
+
+
+#Exercise 3
+mylist=[3,4,5,6]
+for x in mylist:
+    if x==5:
+        break
+    print(x)
+    
