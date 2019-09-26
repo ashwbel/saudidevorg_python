274f09dbec9f962ae0def42a5121eeca63eb3372
ashwbel <ashwag.wabel11@gmail.com>
2019-09-26 20:05:37 +0300

    This Is My Code For DAY 28!!

---
+++ week 5/Day28.py
@@ -0,0 +1,63 @@
+
+
+#day 28
+
+#Exercise 1
+i=1
+while i < 6:
+    print(i)
+    i+=1
+
+
+#Exercise 2
+i=1
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i+=1
+
+
+
+#Exercise 3
+i=1
+while i < 6:
+    i+=1
+    if i == 3:
+        continue
+    print(i)
+
+#Exercise 4
+
+i=1
+while i < 6:
+    print(i)
+    i+=1
+else:
+    print('i is grater than 6 ')
+
+
+
+
+
+#Exercise 4
+a=10
+b=2
+if a > b:print ('a grater than b ')
+
+
+#Exercise 5
+a=10
+b=3
+c=22
+if a > b  and  c > b :
+    print ('both condtion true')
+
+
+
+#Exercise 6
+a=10
+b=3
+c=22
+if a > b  or  c > b :
+    print ('one condtion true')
