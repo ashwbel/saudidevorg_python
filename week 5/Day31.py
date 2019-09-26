2382df267ba89d0368c391f1dc6e9f92536e1ffd
ashwbel <ashwag.wabel11@gmail.com>
2019-09-26 20:07:28 +0300

    This Is My Code For DAY 31!!

---
+++ week 5/Day31.py
@@ -0,0 +1,27 @@
+
+
+#day 31
+
+#Exercise 1
+def myfunc1():
+    print('hello my function')
+
+
+myfunc1()
+
+#Exercise 2
+def myfunc2(name):
+    print('hello'+ name)
+
+myfunc2('ASHWAG')
+myfunc2('WABEL')
+
+
+#Exercise 3
+def myfunc3(AGE =20):
+    print('My Age is '+ name)
+
+
+myfunc3(10)
+myfunc3()
+myfunc3(33)
