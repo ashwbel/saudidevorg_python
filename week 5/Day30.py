d13643b5419edf3a041dcca4c8a622f6439a5587
ashwbel <ashwag.wabel11@gmail.com>
2019-09-26 20:06:44 +0300

    This Is My Code For DAY 30!!

---
+++ week 5/Day30.py
@@ -0,0 +1,29 @@
+
+
+#day 30
+
+#Exercise 1
+for x in rang(10):
+    print(x)
+
+
+#Exercise 2
+for x in rang(4 ,20,2):
+    print(x)
+
+
+#Exercise 3
+
+for x in rang(10):
+    print(x)
+else:
+    print('finish')
+
+
+#Exercise 4
+mynum=[3,4,5,6,7,8]
+mychar=['A','B','C','D','E']
+
+for x in mynum:
+    for y in mychar:
+        print(x,y)
