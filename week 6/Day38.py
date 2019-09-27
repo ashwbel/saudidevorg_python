e353e4259c21aeb7a2f025596ef102bd0d93043d
ashwbel <ashwag.wabel11@gmail.com>
2019-09-27 16:53:04 +0300

    This Is My Code For DAY 38!!

---
+++ week 6/Day38.py
@@ -0,0 +1,21 @@
+
+
+#day 38
+
+#Exercise 1
+num= [1,2,3,4,5,6,7,8]
+
+for x in num:
+    print (x)
+
+
+#Exercise 2
+num= [1,2,3,4,5,6,7,8]
+num.append(10)
+print (num)
+
+
+#Exercise 3
+num= [1,2,3,4,5,6,7,8]
+num.pop(3)
+print (num)
