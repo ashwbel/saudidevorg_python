63b8ca536f4128b07ee00583aeefeef2984a5195
ashwbel <ashwag.wabel11@gmail.com>
2019-09-27 16:47:25 +0300

    This Is My Code For DAY 37!!

---
+++ week 6/Day37.py
@@ -0,0 +1,24 @@
+
+
+#day 37
+
+#Exercise 1
+num= [1,2,3,4,5,6,7,8]
+
+print (num)
+
+#Exercise 2
+num= [1,2,3,4,5,6,7,8]
+
+print (num[4])
+
+
+#Exercise 3
+num= [1,2,3,4,5,6,7,8]
+num[4]=9
+print (num)
+
+#Exercise 4
+num= [1,2,3,4,5,6,7,8]
+
+print (len(num))
